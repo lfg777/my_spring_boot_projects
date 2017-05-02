bdf9e39ac1efcf4174b3dcf650b8523548915937
李风光 <lifengguang@ppdai.com>
2017-05-02 16:11:25 +0800

    对象数组排序

--- src/main/java/com/lfg/beans/LambdaComparator.java
+++ src/main/java/com/lfg/beans/LambdaComparator.java
@@ -1,40 +1,41 @@
 package com.lfg.beans;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.function.Function;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
+ * jdk8新特性
  * Created by lifengguang on 2017/4/19.
  */
 public class LambdaComparator {
 
     public static void main(String[] args) {
+        LambdaComparator lc = new LambdaComparator();
+        lc.sortReversed();
+        lc.filters();
+        lc.delOneFromList();
+    }
+
+
+
+
+    /**
+     * 解释:从字符串序列中过滤出以字符a开头的字符串并迭代打印输出
+     */
+    public void filters(){
         List<Person> peoples = buildList();
-        System.out.println(peoples);
-        Function<Person, Integer> getAge = Person::getAge;
-        Comparator<Person> byAge = Comparator.comparing(getAge);
-        Comparator<Person> tComparator = Comparator.nullsFirst(byAge);
-
-        //升序-age实现了Comparable接口
-        List<Person> collect = peoples.stream().sorted(byAge).collect(Collectors.toList());
-        System.out.println(collect);
-
-        //降序-age实现了Comparable接口
-        List<Person> collect1 = peoples.stream().sorted(tComparator.reversed()).collect(Collectors.toList());
-        System.out.println(collect1);
-
-        //升序-name没有实现Comparable接口
-        /*Comparator<Address> cmpAddr = Comparator.comparing(Address::getAddress);
-        Comparator<Person> byAddress = Comparator.comparing(Person::getAddress, cmpAddr);
-        List<Student> sortedAddressList = students.stream().sorted(byAddress).collect(Collectors.toList());
-        System.out.println(sortedAddressList);*/
-        System.out.println("============");
-        // 解释:从字符串序列中过滤出以字符a开头的字符串并迭代打印输出
         peoples.stream().filter((s)->s.getName().startsWith("d")).forEach(System.out::println);
+        System.out.println("===");
+    }
 
+    /**
+     * 降序排列
+     */
+    public void sortReversed(){
+        List<Person> peoples = buildList();
+        List<Person> sortedList = peoples.stream().sorted(Comparator.nullsLast(Comparator.comparing(Person::getAge)).reversed()).collect(Collectors.toList());
+        System.out.println(sortedList);
+        System.out.println("===");
     }
 
     public  static List<Person> buildList(){
@@ -47,5 +48,38 @@ public class LambdaComparator {
         Person p6 = new Person("d343",9);
         List<Person> list = Arrays.asList(p1, p2, p3, p4,p5,p6);
         return list;
+    }
+
+    public static List<Person> buildArrayList(){
+        Person p1 = new Person("a",3);
+        Person p2 = new Person("b",4);
+        Person p3 = new Person("c",1);
+        Person p4 = new Person("d",5);
+        Person p5 = new Person("deeww",6);
+        Person p6 = new Person("d343",9);
+        Person p7 = new Person("wddlakd",6);
+        List<Person> peoples = new ArrayList<>();
+        peoples.add(p1);
+        peoples.add(p2);
+        peoples.add(p3);
+        peoples.add(p4);
+        peoples.add(p5);
+        peoples.add(p6);
+        return peoples;
+    }
+
+    public void delOneFromList(){
+        //不能用Arrays.asList(); iterator.remove()在list中默认抛出异常
+        List<Person> peoples = buildArrayList();
+        Iterator<Person> iterator = peoples.iterator();
+        while (iterator.hasNext()) {
+            Person p = iterator.next();
+            if (p.getAge() == 6) {
+                iterator.remove();
+            }
+        }
+        System.out.println(peoples);
+        System.out.println("===");
+
     }
 }

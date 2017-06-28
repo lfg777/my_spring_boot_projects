585461970f09666f573b755a06d7e2139ef29cd5
李风光 <lifengguang@ppdai.com>
2017-06-28 11:07:51 +0800

    spring-boot-thymeleaf

--- src/main/java/com/lfg/common/aspect/WebLogAspect.java
+++ src/main/java/com/lfg/common/aspect/WebLogAspect.java
@@ -4,7 +4,6 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -26,12 +25,7 @@ public class WebLogAspect {
 
     private ThreadLocal<Long> startTime = new ThreadLocal<Long>();
 
-    @Pointcut("execution(public * com.lfg.controller..*.*(..))")
-    public void log(){
-
-    }
-
-    @Before("log()")
+    @Before("execution(public * com.lfg.controller..*.*(..))")
     public void doBefore(JoinPoint joinPoint){
         startTime.set(System.currentTimeMillis());
         ServletRequestAttributes attributes = (ServletRequestAttributes)RequestContextHolder.getRequestAttributes();
@@ -41,7 +35,7 @@ public class WebLogAspect {
 
     }
 
-    @AfterReturning(returning = "ret",pointcut = "log()")
+    @AfterReturning(returning = "ret",pointcut = "execution(public * com.lfg.controller..*.*(..))")
     public void doAfterReturning(Object ret) throws Throwable{
         logger.info("响应信息:【"+ ret+"耗时:"+(System.currentTimeMillis()-startTime.get())+"ms】");
     }

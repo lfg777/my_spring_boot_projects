cde90b3966c2ec1b5cb9e03d3fcc9b694fc32d63
李风光 <lifengguang@ppdai.com>
2018-01-12 18:34:43 +0800

    classLoader 加载机制

--- src/main/java/com/lfg/classLoader/MyClassLoader.java
+++ src/main/java/com/lfg/classLoader/MyClassLoader.java
@@ -8,6 +8,12 @@ public class MyClassLoader extends ClassLoader {
 
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
+        Class clazz = super.findLoadedClass(name);
 
+        if (clazz == null) {
+
+
+        }
+        return clazz;
     }
 }

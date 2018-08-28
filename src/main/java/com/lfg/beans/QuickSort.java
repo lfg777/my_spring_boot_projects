43b0bb306126b06667eb4e468f6dba1491d2b055
李风光 <lifengguang@ppdai.com>
2018-08-28 14:46:13 +0800

    快速排序算法

--- src/main/java/com/lfg/beans/QuickSort.java
+++ src/main/java/com/lfg/beans/QuickSort.java
@@ -11,33 +11,45 @@ public class QuickSort {
 
     /**
      * 快速排序算法,固定基准点
-     * @param list
-     * @param left
-     * @param right
+     *
+     *
      */
-    public void quickSort(int[] list,int left,int right) {
-        if (null == list || list.length <= 1) {
+    public static void quickSort(int[] list,int left,int right) {
+        if (null == list || list.length <= 1 || left>right) {
             return;
         }
-        int base = list[0],begin=left,end=right;
+        int base = list[left],i=left,j=right;
 
-
-
-
-
-
-        while (begin < end && list[end] <= base) {
-            while (begin < end && list[begin] >= base) {
-                int temp = list[begin];
-                list[begin] = list[end];
-                list[end] = temp;
+        while (i < j) {
+            while (i < j && list[j] >= base) {
+                j--;
+            }
+            while (i < j && list[i] <= base) {
+                i++;
+            }
+            if (i < j) {
+                int temp = list[i];
+                list[i] = list[j];
+                list[j]=temp;
             }
         }
+        list[left] = list[i];
+        list[i] = base;
+        quickSort(list,left,i-1);
+        quickSort(list,i+1,right);
 
+    }
 
+    public static void sort(int[] a) {
+        quickSort(a, 0, a.length - 1);
+        for (int i : a) {
+            System.out.println(i+"");
+        }
+    }
 
-
-
+    public static void main(String[] args) {
+        int[] a = { 49, 38, 65, 97, -76, 13, 27, 50 };
+        sort(a);
     }
 
 

509b2ec6edde254a9788f4d062410b9dcf7664a2
李风光 <lifengguang@ppdai.com>
2019-05-16 17:03:19 +0800

    栈实现队列

---
+++ src/main/java/com/lfg/datastructure/StackQueueStructure.java
@@ -0,0 +1,22 @@
+package com.lfg.datastructure;
+
+import java.util.Stack;
+
+/**
+ * 栈实现队列,两个栈即可实现队列
+ * @author fg.Li
+ * 2019/5/15:17:19
+ * <p>
+ **/
+public class StackQueueStructure<E> {
+
+    private transient Stack<E> stack1 = new Stack();
+
+    private transient Stack<E> stack2 = new Stack();
+
+
+
+
+
+
+}
